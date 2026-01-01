f6b0287ca2d919b6c6091baed5ad8fb413d5cb29
snow___shower <wonsb25@gmail.com>
2026-01-01 19:49:20 +0900

    feat: 로또 당첨 정보를 가진 enum 클래스 작성

---
+++ src/main/java/lotto/domain/LottoInfo.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+public enum LottoInfo {
+
+    FIRST(6, false, 2000000000L),
+    SECOND(5, true, 30000000L),
+    THIRD(5, false, 1500000L),
+    FOURTH(4, false, 50000L),
+    FIFTH(3, false, 5000L),
+    MISS(0,false, 0L);
+
+    private final int matchNumbers;
+    private final boolean bonusMatch;
+    private final Long prize;
+
+    LottoInfo(int matchNumbers, boolean bonusMatch, Long prize){
+        this.matchNumbers=matchNumbers;
+        this.bonusMatch=bonusMatch;
+        this.prize=prize;
+    }
+
+}
---
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class LottoOutputView {
+
+    public void outputReceivedLotto(){
+
+    }
+}
