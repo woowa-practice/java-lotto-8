8c8f20662582e8db7da605d11e0cc96650e97fb4
snow___shower <wonsb25@gmail.com>
2026-01-01 19:38:01 +0900

    feat: 입력 뷰 구현

---
+++ src/main/java/lotto/view/LottoInputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class LottoInputView {
+
+    public Integer inputLottoAmount() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public String inputLottoNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public Integer inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+}
