f024328149e95a416c8cb0d90e882e640a8740c0
snow___shower <wonsb25@gmail.com>
2026-01-09 22:56:45 +0900

    refactor: 검증 로직 추가

--- src/main/java/lotto/domain/WinningInfo.java
+++ src/main/java/lotto/domain/WinningInfo.java
@@ -4,20 +4,27 @@ public class WinningInfo {
     private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public WinningInfo(Lotto winningLotto, int bonusNumber){
-        this.winningLotto=winningLotto;
-        this.bonusNumber=bonusNumber;
+    public WinningInfo(Lotto winningLotto, int bonusNumber) {
+        validate(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
     }
 
-    public Rank calculateRank(Lotto lotto){
-        int count=(int) winningLotto.getNumbers().stream()
+    public Rank calculateRank(Lotto lotto) {
+        int count = (int) winningLotto.getNumbers().stream()
                 .filter(lotto.getNumbers()::contains)
                 .count();
-        boolean bonusMatch=lotto.getNumbers().contains(bonusNumber);
+        boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
         return Rank.from(count, bonusMatch);
     }
 
-    public Lotto getWinningLotto(){
+    private void validate(Lotto winningLotto, int bonusNumber) {
+        if (!winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Lotto getWinningLotto() {
         return winningLotto;
     }
 
