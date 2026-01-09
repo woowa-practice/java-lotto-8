80ea8c5a6f31dbea7f340aa7e4bde71188483ea0
snow___shower <wonsb25@gmail.com>
2026-01-09 18:17:06 +0900

    refactor: 입출력 양식 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,12 +11,12 @@ public class InputView {
 
     //여기서 리스트로 변환 후 반환해도 되는지?
     public String inputWinningLotto(){
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public Integer inputBonusNumber(){
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,19 +1,25 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+
 import java.util.List;
 
 public class OutputView {
 
-    public void outputRandomLotto(Integer amount, List<String> lottos){
-        System.out.println(amount+"개를 구매했습니다.");
-        for (String lotto : lottos) {
-            System.out.println(lotto);
+    public void outputRandomLotto(Integer count, List<Lotto> lottos){
+        System.out.println("\n"+count+"개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
         }
     }
 
-    public void outputLottoResult(){
+    public void outputLottoResult(LottoResult result){
         System.out.println("당첨 통계");
         System.out.println("---");
 
+        System.out.println(result.getResults());
+
+        System.out.println("총 수익률은 "+result.getYield()+"%입니다.");
     }
 }
