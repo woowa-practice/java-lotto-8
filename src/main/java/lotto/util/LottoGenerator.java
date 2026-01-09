256148f3a79483bb61303ee0bfb997f0885ccbf0
snow___shower <wonsb25@gmail.com>
2026-01-09 16:35:08 +0900

    feat: 랜덤한 6개 숫자 반환하는 로직 구현

---
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int START=1;
+    private static final int END=45;
+    private static final int COUNT=6;
+
+    public List<Integer> generateLotto(){
+        return Randoms.pickUniqueNumbersInRange(START, END, COUNT);
+    }
+}
