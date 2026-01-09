b100acb238f15b65c84b7f45bee3e00f948fa3bc
snow___shower <wonsb25@gmail.com>
2026-01-09 19:39:35 +0900

    refactor: 중복 검사 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,7 +8,6 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -16,10 +15,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        isDuplicated(numbers);
+    }
+
+    private void isDuplicated(List<Integer> numbers){
+        if(numbers.size()!=numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers(){
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 }
