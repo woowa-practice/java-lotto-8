aa667af15b220cec2c353a920576ef60f21e8135
snow___shower <wonsb25@gmail.com>
2026-01-09 18:36:46 +0900

    refactor: 출력 양식 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
+import lotto.domain.Rank;
 
 import java.util.List;
 
@@ -15,10 +16,17 @@ public class OutputView {
     }
 
     public void outputLottoResult(LottoResult result){
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println("\n당첨 통계" +
+                "\n---");
 
-        System.out.println(result.getResults());
+        for (Rank rank : result.getResults().keySet()) {
+            if(rank==Rank.MISS) continue;
+            System.out.println(rank.getDescription()+result.getResults().get(rank)+"개");
+//            System.out.print(rank.getMatchCount()+"개 일치 (");
+//            System.out.printf("%,d", rank.getPrize());
+//            System.out.print("원) - "+result.getResults().get(rank)+"개");
+//            System.out.println();
+        }
 
         System.out.println("총 수익률은 "+result.getYield()+"%입니다.");
     }
